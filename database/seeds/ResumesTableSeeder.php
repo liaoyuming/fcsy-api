66e6aa1dcbb14bec5949a44d5c9db08a6b63cddd
sky <mingyuliao@foxmail.com>
2017-06-19 14:19:45 +0800

    ADD seeder

---
+++ database/seeds/ResumesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class ResumesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::first();
+        
+        DB::table('Resumes')->insert([
+            'user_id' => $user->id,
+            'name' => $user->username,
+            'sex' => rand(0, 1),
+            'position' => '程序员',
+            'city' => '北京',
+            'mobile' => $user->mobile,
+            'email' => str_random(10) . '@gmail.com',
+            'value' => rand(4100, 9600),
+        ]);
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+           'username' => 'test' . str_random(4),
+           'mobile' => '135'. rand(10000000, 99999999),
+           'password' => bcrypt('secret'),
+           'is_member' => 1,
+           'member_expire_time' => date('Y-m-d h:i:s', strtotime('+1 year')),
+           'created_at' => date('Y-m-d h:i:s', time()),
+           'updated_at' => date('Y-m-d h:i:s', time()),
+       ]);
+    }
+}
