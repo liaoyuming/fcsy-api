e21d575358d5515db7a0ab9afa0cd78cabe1c172
sky <mingyuliao@foxmail.com>
2017-06-19 14:18:37 +0800

    ADD dingo

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Shanghai',
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Dingo\Api\Provider\LaravelServiceProvider::class
 
     ],
 
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+$api = app('Dingo\Api\Routing\Router');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api->version('v1', function ($api) {
+    $api->group(['namespace' => 'App\Http\ApiControllers'], function ($api) {
+
+        $api->get('users', 'UserController@index');
+        $api->get('resume', 'ResumeController@index');
+
+
+    });
 });
