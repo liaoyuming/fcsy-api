400ad6144641f7bfbb73996acdb89be7e7eff7aa
sky <mingyuliao@foxmail.com>
2017-06-28 22:08:27 +0800

    Update user_question table

--- database/migrations/2017_06_23_102705_create_user_question_table.php
+++ database/migrations/2017_06_23_102705_create_user_question_table.php
@@ -18,9 +18,9 @@ class CreateUserQuestionTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->integer('question_id')->unsigned();
-            $table->integer('character_type_id')->unsigned();
+            $table->integer('question_option_id')->unsigned();
             $table->foreign('question_id')->references('id')->on('questions');
-            $table->foreign('character_type_id')->references('id')->on('character_types');
+            $table->foreign('question_option_id')->references('id')->on('question_options');
         });
     }
 
